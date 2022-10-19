e8e002da5f720b16549d7868535da78f1e16b22a
adrianfi <pfirouzmakan@gmail.com>
2022-10-19 16:22:42 -0400

    needs to wokr on

--- XML_File_Reader.py
+++ XML_File_Reader.py
@@ -1,11 +1,22 @@
 """
+<<Main>>
+
 This Module is coded for extracting data from XML file related to ..., ..., ....
 The output Data Structure is :....
 """
-import xml.etree.ElementTree as ET
+
+import xml.dom.minidom
 
 __author__ = "Adrian (Pouya) Firouzmakan"
 __all__ = []
 
-my_tree = ET.parse('sumoTrace_test.xml')
-root = my_tree.getroot()
+
+my_tree = xml.dom.minidom.parse("sumoTrace_test.xml")
+fcd = my_tree.documentElement
+times = fcd.getElementsByTagName('timestep')
+
+for each_second in times[0:11]:
+    print('-------------------')
+    print(f"Second: {each_second.getAttribute('time')}")
+    for att in each_second.childNodes[1::2]:
+        print(f"---- id: {att.getAttribute('id')}")
