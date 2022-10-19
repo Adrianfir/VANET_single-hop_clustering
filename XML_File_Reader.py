4f5e8326dd689c2d69b408295c41f14e9adc909a
adrianfi <pfirouzmakan@gmail.com>
2022-10-19 12:50:04 -0400

    Just to Test

--- XML_File_Reader.py
+++ XML_File_Reader.py
@@ -2,7 +2,10 @@
 This Module is coded for extracting data from XML file related to ..., ..., ....
 The output Data Structure is :....
 """
+import xml.etree.ElementTree as ET
 
 __author__ = "Adrian (Pouya) Firouzmakan"
 __all__ = []
+
+my_tree = ET.parse('sumoTrace_test.xml')
 
