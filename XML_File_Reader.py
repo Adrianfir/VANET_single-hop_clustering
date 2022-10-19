31674df2d59cdda2241bf058a579d83b3d74bfce
Adrianfir <pfirouzmakan@gmail.com>
2022-10-19 12:06:46 -0400

    Add files via upload

---
+++ XML_File_Reader.py
@@ -0,0 +1,8 @@
+"""
+This Module is coded for extracting data from XML file related to ..., ..., ....
+The output Data Structure is :....
+"""
+
+__author__ = "Adrian (Pouya) Firouzmakan"
+__all__ = []
+
---
+++ test_and_try.py
@@ -0,0 +1,5 @@
+import xml.etree.ElementTree as ET
+
+my_tree = ET.parse("/Users/pouyafirouzmakan/Desktop/project/sumoTrace.xml")
+
+a = 2
