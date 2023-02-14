fb6b69d411a4a75b405d9bd063c84efc0d332a97
Pouya Firouzmakan <pouyafirouzmakan@g76-u17-v60x.mgmt.yorku.ca>
2023-02-14 16:56:34 -0500

    it seems I have done all the cleaning

--- utils/util.py
+++ utils/util.py
@@ -6,6 +6,10 @@ import random
 
 
 def mac_address():
+    """
+    This function is used in the Main.py file
+    :return: this function will return a generated random MAC Address for ehicles
+    """
     mac = [152, 237, 92,
            random.randint(0x00, 0x7f),
            random.randint(0x00, 0xff),
@@ -17,13 +21,13 @@ def middle_zone(u_row, u_col,
                 l_row, l_col,
                 n_cols):
     """
-
+    This function is used in the Zone.py file
     :param u_row: The upper row id +1
     :param l_row: The lower row id +1
     :param u_col: The upper col id +1
     :param l_col: The lower col id +1
     :param n_cols: The maximum number of columns in the whole area
-    :return:
+    :return: The zone in the middle of the inputs
     """
     # the almost centre zone id will be obtained here
     middle_row = int(np.floor((u_row + l_row) / 2))
