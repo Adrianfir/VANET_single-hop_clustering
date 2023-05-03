bd20ba1489ef6d350f5457f927d0aa0ce816a75c
Adrianfir <pfirouzmakan@gmail.com>
2023-05-03 15:03:40 -0400

    working on main.py

--- Main.py
+++ Main.py
@@ -12,17 +12,18 @@ from configs.config import Configs
 from Zone import ZoneID
 
 
-configs = Configs().config
+if __name__ == "__main__":
+    configs = Configs().config
 
-area_zones = ZoneID(configs)  # This is a hash table including all zones and their max and min lat and longs
-area_zones.zones()
+    area_zones = ZoneID(configs)  # This is a hash table including all zones and their max and min lat and longs
+    area_zones.zones()
 
-a = DataTable(configs, area_zones)
-a.print_table()
-a.update(configs, area_zones)
-for i in a.veh_table.ids():
-    a.update_cluster(i, configs, area_zones)
-print('bus-ids: ', a.bus_table.ids())
-print('vehicles-ids: ', a.veh_table.ids())
-print('\n')
-a.print_table()
+    a = DataTable(configs, area_zones)
+    a.print_table()
+    a.update(configs, area_zones)
+    for i in a.veh_table.ids():
+        a.update_cluster(i, configs, area_zones)
+    print('bus-ids: ', a.bus_table.ids())
+    print('vehicles-ids: ', a.veh_table.ids())
+    print('\n')
+    a.print_table()
