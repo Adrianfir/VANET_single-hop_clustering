2aea0ce93c1c359427be50e9718bc018974bf69c
Adrianfir <pfirouzmakan@gmail.com>
2023-10-24 12:05:49 -0400

    rsu data generator is fixed

--- create_rsu_data.py
+++ create_rsu_data.py
@@ -1,43 +1,29 @@
-"""
-This .py file is for adding RSUs to dataset and creating a new .xml file
-"""
-__author__: str = "Pouya 'Adrian' Firouzmakan"
-
 import xml.etree.ElementTree as ET
 
-
-def add_vehicle_to_xml(input_file, output_file, vehicle_element):
+def add_vehicle_to_xml(input_file, output_file, vehicle_elements):
     # Parse the input XML file
     tree = ET.parse(input_file)
-    root = tree.getroot()
-    for i in vehicle_element:
-        # Iterate through all <timestep> elements
-        for timestep in root.findall(".//timestep"):
-            # Create a copy of the vehicle_element
-            new_vehicle = ET.Element(i.tag, i.attrib)
 
-            # Append the new vehicle element to the timestep
-            timestep.append(new_vehicle)
+    # Iterate through each timestep
+    for timestep in tree.findall(".//timestep"):
+        for vehicle_element in vehicle_elements:
+            # Create a new vehicle element and add it to the timestep
+            new_vehicle = ET.SubElement(timestep, vehicle_element.tag, vehicle_element.attrib)
+            new_vehicle.tail = "\n        "  # Add proper indentation
 
     # Write the modified XML to the output file
-    tree.write(output_file)
+    tree.write(output_file, encoding='utf-8', xml_declaration=True)
 
-
-# Define the vehicle element to add
-new_vehicle_element = [
-    # ET.fromstring('''
-    # <vehicle id="busrsu1" x="-79.437512" y="43.871043" angle="71.93" type="veh_passenger"
-    # speed="0.00" pos="5.10" lane="35349093_0" slope="0.00"/>''')
-                       ET.fromstring('''
-                           <vehicle id="busrsu2" x="-79.448788" y="43.868880" angle="71.93" type="veh_passenger"
-                           speed="0.00" pos="5.10" lane="35349093_0" slope="0.00"/>'''),
-                       ET.fromstring('''
-                           <vehicle id="busrsu3" x="-79.435327" y="43.863937" angle="71.93" type="veh_passenger"
-                           speed="0.00" pos="5.10" lane="35349093_0" slope="0.00"/>''')
-                       ]
+# Define the vehicle elements to add
+new_vehicle_elements = [
+    ET.fromstring('''
+        <vehicle angle="71.93" id="busrsu2" lane="35349093_0" pos="5.10" slope="0.00" speed="0.00" type="veh_passenger" x="-79.448788" y="43.868880" />'''),
+    ET.fromstring('''
+        <vehicle angle="71.93" id="busrsu3" lane="35349093_0" pos="5.10" slope="0.00" speed="0.00" type="veh_passenger" x="-79.435327" y="43.863937" />''')
+]
 
 input_file = "/Users/pouyafirouzmakan/Desktop/VANET/final_data_Richmondhill/sumoTrace.xml"
 output_file = "/Users/pouyafirouzmakan/Desktop/VANET/final_data_Richmondhill/sumoTrace_rsu.xml"
 
-# Call the function to add the vehicle element to all timesteps
-add_vehicle_to_xml(input_file, output_file, new_vehicle_element)
+# Call the function to add the vehicle elements to all timesteps
+add_vehicle_to_xml(input_file, output_file, new_vehicle_elements)
