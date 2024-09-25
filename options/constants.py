58b1e3e19ee2e46933d5099cead9865795dcfc0e
Pouya Firouzmakan <pouyafirouzmakan@1-e84-u18-x2200.mgmt.yorku.ca>
2024-09-24 20:33:17 -0400

    there was a very bad issue in removing vehivles in update in data_cluster and it seems its fixed now

--- options/constants.py
+++ options/constants.py
@@ -13,12 +13,12 @@ import xml.dom.minidom
 class Inputs:
     def __init__(self):
         # Constants that we need to pass as arguments
-        trace_path = str(pathlib.Path(__file__).parent.parent.absolute().
-                         joinpath('final_data_Richmondhill', 'sumoTrace.xml'))
-        sumo_edge_path = str(pathlib.Path(__file__).parent.parent.absolute().
-                             joinpath('final_data_Richmondhill', 'osm.net.xml'))
-        sumo_node_path = str(pathlib.Path(__file__).parent.parent.absolute().
-                             joinpath('final_data_Richmondhill', 'osm_bbox.osm.xml'))
+        trace_path = str(pathlib.Path(__file__).parent.parent.parent.absolute().
+                         joinpath('final_mhc_data', 'sumoTrace.xml'))
+        sumo_edge_path = str(pathlib.Path(__file__).parent.parent.parent.absolute().
+                             joinpath('final_mhc_data', 'osm.net.xml'))
+        sumo_node_path = str(pathlib.Path(__file__).parent.parent.parent.absolute().
+                             joinpath('final_mhc_data', 'osm_bbox.osm.xml'))
         sumo_trace = xml.dom.minidom.parse(trace_path)
         sumo_edge = xml.dom.minidom.parse(sumo_edge_path)
         sumo_node = xml.dom.minidom.parse(sumo_node_path)
@@ -28,17 +28,17 @@ class Inputs:
                     min_long=-79.540771,
                     max_lat=44.012923,
                     max_long=-79.238069)
-        alpha = 0.8
-        veh_trans_range = 400
+        alpha = 0.5
+        veh_trans_range = 300
         bus_trans_range = 800
         start_time = 1600
-        iter = 120
+        iter = 60
         counter = 4
         priority_counter = 100   # this is not used for decision-making to join a cluster in single-hop algorithm
         map_zoom = 15.3
         center_loc = [43.869846, -79.443523]
         fps = 5
-        weights = np.array([0, 1, 0])      # direction's angle, speed, distance
+        weights = np.array([0.5, 0, 0.5])      # direction's angle, speed, distance
 
         parser = argparse.ArgumentParser()
         parser.add_argument('--area', type=dict, default=area,
