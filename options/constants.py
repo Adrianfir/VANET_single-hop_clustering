4c8de622a4a860a72a087607fb0337ef800662a3
Pouya Firouzmakan <pouyafirouzmakan@e75-u02-x2100.mgmt.yorku.ca>
2024-11-04 11:57:32 -0500

    nothing

--- options/constants.py
+++ options/constants.py
@@ -14,11 +14,11 @@ class Inputs:
     def __init__(self):
         # Constants that we need to pass as arguments
         trace_path = str(pathlib.Path(__file__).parent.parent.absolute().
-                         joinpath('final_data_Richmondhill', 'sumoTrace_no_bus_and_rsu.xml'))
+                         joinpath('final_data_Richmondhill_midsize', 'sumoTrace_no_bus_and_rsu.xml'))
         sumo_edge_path = str(pathlib.Path(__file__).parent.parent.absolute().
-                             joinpath('final_data_Richmondhill', 'osm.net.xml'))
+                             joinpath('final_data_Richmondhill_midsize', 'osm.net.xml'))
         sumo_node_path = str(pathlib.Path(__file__).parent.parent.absolute().
-                             joinpath('final_data_Richmondhill', 'osm_bbox.osm.xml'))
+                             joinpath('final_data_Richmondhill_midsize', 'osm_bbox.osm.xml'))
         sumo_trace = xml.dom.minidom.parse(trace_path)
         sumo_edge = xml.dom.minidom.parse(sumo_edge_path)
         sumo_node = xml.dom.minidom.parse(sumo_node_path)
@@ -29,7 +29,7 @@ class Inputs:
                     max_lat=44.012923,
                     max_long=-79.238069)
         alpha = 0.5
-        veh_trans_range = 300
+        veh_trans_range = 100
         bus_trans_range = 800
         start_time = 1600
         iter = 60
