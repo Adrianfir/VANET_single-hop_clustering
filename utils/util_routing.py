e3f2c935465a78c914c1dc1a91daa9bfb32bb491
Pouya Firouzmakan <pouyafirouzmakan@e75-u21-v245.mgmt.yorku.ca>
2025-10-02 14:28:17 -0400

    just staerted

---
+++ utils/util_routing.py
@@ -0,0 +1,25 @@
+"""
+This is the utils file including the small functions for basic routing implementation
+using https://ieeexplore.ieee.org/abstract/document/8588189
+"""
+__author__: str = "Pouya 'Adrian' Firouzmakan"
+__all__ = [
+
+           ]
+
+import numpy as np
+import random
+import haversine as hs
+from linked_list import LinkedList
+from scipy import spatial
+import time
+from PIL import Image
+from io import BytesIO
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+import os
+import cv2
+import re
+
+
